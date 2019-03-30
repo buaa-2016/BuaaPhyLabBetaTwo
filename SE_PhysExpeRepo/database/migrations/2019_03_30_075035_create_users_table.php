e7d2e88157282235df5ae17ae76903371fff790c
fondoger <fondoger@outlook.com>
2019-03-30 16:22:16 +0800

    tmd全是bug

---
+++ SE_PhysExpeRepo/database/migrations/2019_03_30_075035_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('email')->unique();
+            $table->string('student_id')->unique();
+            $table->string('password', 60);
+            $table->rememberToken();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->enum('sex',['male','female','secret'])->nullable();
+            $table->string('company')->nullable();
+            $table->string('company_addr')->nullable();
+            $table->date('birthday')->nullable();
+            $table->text('introduction')->nullable();
+            $table->string('avatar_path')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
---
+++ SE_PhysExpeRepo/database/migrations/2019_03_30_081053_create_stars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('link');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('report_id')->unsigned()->index();
+            $table->foreign('report_id')->references('id')->on('reports')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('stars');
+    }
+}
